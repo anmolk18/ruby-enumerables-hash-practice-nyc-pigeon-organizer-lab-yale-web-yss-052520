46369d606df0d6f25df039831d9493f7a37bb1ec
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-15 00:32:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,18 @@
 def nyc_pigeon_organizer(data)
-new_hash = {}
-  data.each do |property, hash|
-    hash.each do |attribute, array|
+new_organizer = {}
+  data.each do |attribute, hash|
+    hash.each do |type, array|
       array.each do |name|
-        if !new_hash.has_key?(name)
-          new_hash[name] = {}
+        
+        if !new_organizer.has_key?(name)
+          new_organizer[name] = {}
         end
 
-        if !new_hash[name].has_key?(property)
+        if !new_organizer[name].has_key?(attribute)
           new_hash[name][property] = []
         end
 
-        if !new_hash[name][property].include?(attribute)
+        if !new_hash[name][property].include?(type)
           new_hash[name][property] << attribute.to_s
         end
       end
